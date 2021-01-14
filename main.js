33a4d44c2e2b89269620a4801ed9d59a0e442d2b
Pozharov Roman <ru.roman.ps@gmail.com>
2021-01-14 15:34:00 +0400

    Изменил KEY_CODES и switch в ф-ии handleKeys

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
 "use strict";
-// коды всех клавиш, используемых в калькуляторе
+
+// коды клавиш
 const KEY_CODES = {
-  Digit0: 48,
-  Digit1: 49,
-  Digit2: 50,
-  Digit3: 51,
-  Digit4: 52,
-  Digit5: 53,
-  Digit6: 54,
-  Digit7: 55,
-  Digit8: 56,
-  Digit9: 57,
-  Minus: 189,
-  Numpad0: 96,
-  Numpad1: 97,
-  Numpad2: 98,
-  Numpad3: 99,
-  Numpad4: 100,
-  Numpad5: 101,
-  Numpad6: 102,
-  Numpad7: 103,
-  Numpad8: 104,
-  Numpad9: 105,
-  Equal: 187,
-  NumpadMultiply: 106,
-  NumpadSubtract: 109,
-  NumpadAdd: 107,
-  NumpadDivide: 111,
-  NumpadEnter: 13,
-  Escape: 27,
-  Backspace: 8,
-  Enter: 13,
-  NumpadEnter: 13,
+  Digit0: "0",
+  Digit1: "1",
+  Digit2: "2",
+  Digit3: "3",
+  Digit4: "4",
+  Digit5: "5",
+  Digit6: "6",
+  Digit7: "7",
+  Digit8: "8",
+  Digit9: "9",
+  Numpad0: "0",
+  Numpad1: "1",
+  Numpad2: "2",
+  Numpad3: "3",
+  Numpad4: "4",
+  Numpad5: "5",
+  Numpad6: "6",
+  Numpad7: "7",
+  Numpad8: "8",
+  Numpad9: "9",
+  Minus: "-",
+  NumpadMultiply: "*",
+  NumpadSubtract: "-",
+  NumpadAdd: "+",
+  NumpadDivide: "/",
+  NumpadEnter: "NumpadEnter",
+  Escape: "Escape",
+  Backspace: "Backspace",
+  Enter: "Enter",
+  Equal: "Equal",
 };
 
 const BTNS_VALUE = {
@@ -50,28 +50,27 @@ const BTNS_VALUE = {
   btnMinus: "-",
   btnMulti: "*",
   btnSplit: "/",
-  btnReset: " ",
 };
 
 const OUTPUT_FIELD = document.querySelector(".result"); // поле вывода результата
-const MAIN = document.querySelector(".wrapper"); // обертка калькулятора на которую вешается обработчик событий на клики мыши
-let outputResult = ""; // результат
-OUTPUT_FIELD.textContent = outputResult; // вывод результата в поле
+const WRAPPER = document.querySelector(".wrapper"); // обертка
+let outputResult = ""; // результат вычислений
+OUTPUT_FIELD.textContent = outputResult; // вывод результата
 
 // обработка нажатий клавиш
 function handleKeys(evt) {
   if (KEY_CODES.hasOwnProperty(evt.code)) {
-    // если код нажатой клавиши есть в объекте KEYCODE
+    // если код нажатой клавиши есть в KEY_CODES
     switch (evt.code) {
-      case "Backspace":
+      case KEY_CODES.Backspace: // 8
         deleteSymbols(-1);
         break;
-      case "Escape":
+      case KEY_CODES.Escape:
         deleteSymbols(0);
         break;
-      case "Equal":
-      case "Enter":
-      case "NumpadEnter":
+      case KEY_CODES.Equal:
+      case KEY_CODES.Enter:
+      case KEY_CODES.NumpadEnter:
         renderCalc();
         break;
       default:
@@ -81,27 +80,11 @@ function handleKeys(evt) {
   }
 }
 
-/* function resultOutput() {
-  OUTPUT_FIELD.textContent = outputResult;
-} */
-
-// удаляем символы
+// удаляем символы, последний символ если x = -1
 function deleteSymbols(x) {
   outputResult = outputResult.slice(0, x);
   OUTPUT_FIELD.textContent = outputResult;
 }
-
-// удаляем последний символ
-/* function deleteLastSymbol() {
-  outputResult = outputResult.slice(0, -1);
-  OUTPUT_FIELD.textContent = outputResult;
-} */
-
-// очищаем поле вывода
-/* function resetOutput() {
-  outputResult = outputResult.slice(0, 0);
-  OUTPUT_FIELD.textContent = outputResult;
-} */
 
 // вывод результатов
 function renderCalc() {
@@ -111,11 +94,8 @@ function renderCalc() {
 
 // обработка кликов мыши
 function handleClick(evt) {
-  //let btnValue = evt.target.textContent;
   let target = evt.target;
-  let dataId = evt.target.dataset.id;
-  //console.log(btnValue);
-  //console.log(target);
+  let dataId = target.dataset.id;
   if (target.classList.contains("btn")) {
     switch (dataId) {
       case "btnBack":
@@ -133,28 +113,6 @@ function handleClick(evt) {
     }
   }
 }
-
-// обработка кликов мыши
-/* function handleClick(evt) {
-  let target = evt.target;
-  let btnValue = target.textContent;
-  if (target.classList.contains("btn")) {
-    switch (btnValue) {
-      case "-1":
-        deleteLastSymbol();
-        break;
-      case "C":
-        resetOutput();
-        break;
-      case "=":
-        renderCalc();
-        break;
-      default:
-        outputResult += btnValue;
-        OUTPUT_FIELD.textContent = outputResult;
-    }
-  }
-} */
 
 // поиск знаков с высоким приоритетом
 function checkFirstPriority(x) {
@@ -191,4 +149,4 @@ function calculation(x) {
 }
 
 window.addEventListener("keydown", handleKeys);
-MAIN.addEventListener("click", handleClick);
+WRAPPER.addEventListener("click", handleClick);
